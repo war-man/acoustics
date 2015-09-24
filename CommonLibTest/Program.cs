3292a88e1e23717940065c58eef73515a2b855d9
sam210723 <sam210723@hotmail.com>
2015-09-24 19:51:16 +1000

    Stop console from closing on Windows

--- CommonLibTest/Program.cs
+++ CommonLibTest/Program.cs
@@ -6,7 +6,11 @@ namespace CommonLibTest
 	{
 		public static void Main (string[] args)
 		{
+			Console.Title = "CommonLibTest";
+
 			CommonLib.Util.WriteLine ("CommonLib.Util.WriteLine test", ConsoleColor.Blue, ConsoleColor.Gray);
+
+			Console.In.ReadLine(); // Stops console from closing on Windows
 		}
 	}
 }
