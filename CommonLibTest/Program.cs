b4616b06e9e4939830242f5f2389663eabf23d5c
sam210723 <sam210723@hotmail.com>
2015-09-24 19:14:01 +1000

    Add CommonLibTest

---
+++ CommonLibTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonLibTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			CommonLib.Util.WriteLine ("CommonLib.Util.WriteLine test");
+		}
+	}
+}
