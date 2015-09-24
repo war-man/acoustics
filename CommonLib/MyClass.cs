d4c68a90861ad9e111673054127f2e146ec92cb0
sam210723 <sam210723@hotmail.com>
2015-09-24 15:29:39 +1000

    Initial check-in of module Acoustics

---
+++ CommonLib/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CommonLib
+{
+	public class Util
+	{
+		public Util ()
+		{
+			// Constructor
+		}
+
+		public string libInfo()
+		{
+			return "CommonLib.Acoustics \n Version: 0.1, Build: 1";
+		}
+	}
+}
+
