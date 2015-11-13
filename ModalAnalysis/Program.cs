73740a6e0aa66390e5544342fdfa3a4c8a1e0cac
sam210723 <sam210723@hotmail.com>
2015-11-14 00:53:44 +1100

    Add critical frequency calculation

--- ModalAnalysis/Program.cs
+++ ModalAnalysis/Program.cs
@@ -19,10 +19,12 @@ namespace ModalAnalysis
 			CommonLib.Util.Write ("\t\t Length: " + rL.ToString() + "m\n");
 			CommonLib.Util.Write ("\t\t Width:  " + rW.ToString() + "m\n");
 			CommonLib.Util.Write ("\t\t Height: " + rH.ToString() + "m\n");
-			CommonLib.Util.WriteLine ("Speed of sound: " + v.ToString() + "m/s");
+			CommonLib.Util.WriteLine ("Speed of sound: " + v.ToString() + "m/s\n");
+
+			Calculate ();
 		}
 
-		public static void ParseArgs (string[] args)
+		static void ParseArgs (string[] args)
 		{
 			for (int i = 0; i < args.Length; i = i + 2) {
 				switch (args [i]) {
@@ -43,6 +45,47 @@ namespace ModalAnalysis
 					break;
 				}
 			}
+		}
+
+		/// <summary>
+		/// Performs modal analysis of a given room.
+		/// 1. Calculate critical frequency
+		/// 2. Calculate fundamental axial modes
+		/// 3. Calculate modal harmonics up to critical frequency
+		/// 4. Sort harmonics of all axes
+		/// 5. Find coincidences
+		/// 6. Find spacings
+		/// </summary>
+		static void Calculate ()
+		{
+			double rCF = CriticalFrequency ();
+			CommonLib.Util.WriteLine ("Critical Frequency: " + rCF.ToString() + "Hz", ConsoleColor.White, ConsoleColor.DarkGreen);
+		}
+
+		/// <summary>
+		/// Calculates the critical frequency of a given room.
+		/// 
+		/// 1. Calculate the volume of the room
+		/// 2. Calculate the surface area of the room (4 walls + roof + floor, flat planes)
+		/// 3. Calculate the Mean Free Path (MFP)
+		/// 4. Calculate the critical frequency
+		/// </summary>
+		/// <returns>Critical Frequency (as double, in Hz)</returns>
+		static double CriticalFrequency ()
+		{
+			//Calculate room volume
+			double rVol = rL * rW * rH;
+
+			//Calculate room surface area
+			double rS = 2 * ((rL * rW) + (rL * rH) + (rW * rH));
+
+			//Calculate MFP
+			double rMFP = 4*(rVol/rS);
+
+			//Calculate critical frequency
+			double rCF = 1.5 * v/rMFP;
+
+			return rCF;
 		}
 	}
 }
