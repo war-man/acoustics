ae02d14e1a0c70a18382993b56c8506aca4a6db2
sam210723 <sam210723@hotmail.com>
2015-11-14 00:14:06 +1100

    Create ModalAnalysis

---
+++ ModalAnalysis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ModalAnalysis
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			CommonLib.Util.WriteLine ("Modal Analysis");
+			CommonLib.Util.WriteLine ("");
+
+			ParseArgs (args);
+		}
+
+		public static void ParseArgs (string[] args)
+		{
+			for (int i = 0; i < args.Length; i = i + 2) {
+				switch (args [i]) {
+				default:
+					CommonLib.Util.WriteLine ("Invalid argument: " + args [i], ConsoleColor.White, ConsoleColor.Red);
+					break;
+				}
+			}
+		}
+	}
+}
