cf48f99bd3ab4ac784fd19715b53a76c17a988f2
sam210723 <sam210723@hotmail.com>
2015-11-14 00:25:32 +1100

    Add argument parsing

--- ModalAnalysis/Program.cs
+++ ModalAnalysis/Program.cs
@@ -4,18 +4,40 @@ namespace ModalAnalysis
 {
 	class MainClass
 	{
+		public static double rL = 0; //Room length
+		public static double rW = 0; //Room width
+		public static double rH = 0; //Room height
+		public static double v = 344; //Speed of sound (default 344m/s)
+
 		public static void Main (string[] args)
 		{
-			CommonLib.Util.WriteLine ("Modal Analysis");
-			CommonLib.Util.WriteLine ("");
+			CommonLib.Util.WriteLine ("Modal Analysis\n");
 
 			ParseArgs (args);
+
+			CommonLib.Util.WriteLine ("Room Properties:");
+			CommonLib.Util.Write ("\t\t Length: " + rL.ToString() + "m\n");
+			CommonLib.Util.Write ("\t\t Width:  " + rW.ToString() + "m\n");
+			CommonLib.Util.Write ("\t\t Height: " + rH.ToString() + "m\n");
+			CommonLib.Util.WriteLine ("Speed of sound: " + v.ToString() + "m/s");
 		}
 
 		public static void ParseArgs (string[] args)
 		{
 			for (int i = 0; i < args.Length; i = i + 2) {
 				switch (args [i]) {
+				case ("-l"):
+					rL = double.Parse (args [i + 1]);
+					break;
+				case ("-w"):
+					rW = double.Parse (args [i + 1]);
+					break;
+				case ("-h"):
+					rH = double.Parse (args [i + 1]);
+					break;
+				case ("-v"):
+					v = double.Parse (args [i + 1]);
+					break;
 				default:
 					CommonLib.Util.WriteLine ("Invalid argument: " + args [i], ConsoleColor.White, ConsoleColor.Red);
 					break;
